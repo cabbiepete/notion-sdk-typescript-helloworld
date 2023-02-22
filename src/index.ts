cf1863aea5634c821e057a1bff147c771ad6b674
Peter Simmons <cabbiepete@gmail.com>
2023-02-22 16:40:45 +0000

    Read, add item then read again

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,51 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-async function main() {
-  const notion = new Client({
+const database_id = process.env.DATABASE_ID || "DATABASE_ID not set";
+
+function client() {
+  return new Client({
     auth: process.env.NOTION_TOKEN,
   });
+}
 
-  const response = await notion.databases.query({
-    database_id: "FIXME",
+async function read() {
+  const response = await client().databases.query({
+    database_id,
   });
 
   console.log("Got response:", response);
 }
 
-main()
-  .then(() => process.exit(0))
+async function addItem(text: string) {
+  try {
+    const response = await client().pages.create({
+      parent: { database_id },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+    console.log("Success! Entry added.");
+  } catch (error: any) {
+    console.error(error.body);
+  }
+}
+
+read()
+  .then(async () => {
+    await addItem("Yurts in Big Sur, California");
+    await read();
+    process.exit(0);
+  })
   .catch((err) => {
     console.error(err);
     process.exit(1);
